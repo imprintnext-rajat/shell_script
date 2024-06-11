47fd5286ad786aaf3f01937f189bb99d2600419f
imprintnext-rajat <121029291+imprintnext-rajat@users.noreply.github.com>
2024-06-11 16:50:06 +0530

    Update 7max_dir.sh

--- 7max_dir.sh
+++ 7max_dir.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
-LOCATION=/tmp/dir
-FILECOUNT=$(find $LOCATION -maxdepth 1 -type f | wc -l)
-DIRCOUNT=$(find $LOCATION -maxdepth 1 -type d | wc -l)
-echo " files are: $FILECOUNT "
+LOCATION=/root/test
+#FILECOUNT=$(find $LOCATION -mindepth 1 -maxdepth 1 -type f | wc -l)
+DIRCOUNT=$(find $LOCATION -mindepth 1 -maxdepth 1 -type d | wc -l)
+#echo " files are: $FILECOUNT "
 echo "directories are: $DIRCOUNT "
 
-for ((a=$DIRCOUNT; a>8; a--))
+for ((a=$DIRCOUNT; a>7; a--))
 do
 DIRNAME=$(ls -t1 $LOCATION | tail -n 1)
  echo "Directory LOcation : $LOCATION/$DIRNAME"
  sudo rm -rf $LOCATION/$DIRNAME
  echo "Directory Name : $DIRNAME"
  echo " $DIRNAME is delete"
- DIRCOUNT=$(find $LOCATION -maxdepth 1 -type d | wc -l)
+ DIRCOUNT=$(find $LOCATION -mindepth 1 -maxdepth 1 -type d | wc -l)
 
 echo  "Reset-directories are : $DIRCOUNT "
 done
